b8b489defdc98490edeee9587376ce76f4333f67
Nut Pinyo <bomb.np@gmail.com>
2023-07-16 02:15:41 +0700

    add padding config to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,14 +5,16 @@ import { Navbar } from '../Navbar'
 interface LayoutProps {
 	beforeContent?: ReactNode
 	children: ReactNode
+	px?: number
+	py?: number
 }
 
-export const Layout: FC<LayoutProps> = ({ children, beforeContent }) => {
+export const Layout: FC<LayoutProps> = ({ children, beforeContent, px = 4, py = 4 }) => {
 	return (
 		<div className="flex flex-col w-full font-noto-looped">
 			<Navbar />
 			{beforeContent}
-			<div className="p-4">{children}</div>
+			<div className={`px-${px} py-${py}`}>{children}</div>
 		</div>
 	)
 }
--- src/pages/expenses/3/index.tsx
+++ src/pages/expenses/3/index.tsx
@@ -1,14 +1,12 @@
-import { Layout } from "~/components/Layout"
-import { ExpenseLayout } from "~/components/Layout/ExpenseLayout"
+import { ExpenseLayout } from '~/components/Layout/ExpenseLayout'
+import { Layout } from '~/components/Layout/Layout'
 
 const ExpensesPage = () => {
-    return (
-        <Layout>
-            <ExpenseLayout title="ค่าใช้จ่ายทางการแพทย์">
-                {/* insert your code here */}
-            </ExpenseLayout>
-        </Layout>
-    )
+	return (
+		<Layout>
+			<ExpenseLayout title="ค่าใช้จ่ายทางการแพทย์">{/* insert your code here */}</ExpenseLayout>
+		</Layout>
+	)
 }
 
 export default ExpensesPage
