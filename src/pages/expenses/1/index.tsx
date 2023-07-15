676091208f4c0ba69edf138bec3e9ec4eadc01fc
Nut Pinyo <bomb.np@gmail.com>
2023-07-15 23:20:33 +0700

    format

--- src/pages/expenses/1/index.tsx
+++ src/pages/expenses/1/index.tsx
@@ -1,34 +1,29 @@
-import { Layout } from "~/components/Layout"
-import { type Expense, expenses } from "~/constants/expenses"
-import { Table } from "antd"
-import { type ColumnsType } from "antd/es/table"
-import { Navbar } from "~/components/Navbar";
-import { ExpenseLayout } from "~/components/Layout/ExpenseLayout";
+import { type ColumnsType } from 'antd/es/table'
+import { Layout } from '~/components/Layout'
+import { ExpenseLayout } from '~/components/Layout/ExpenseLayout'
+import { type Expense } from '~/constants/expenses'
 
 const columns: ColumnsType<Expense> = [
-  {
-    title: 'Title',
-    dataIndex: 'title',
-    key: 'title',
-  },
-  {
-    title: 'Description',
-    dataIndex: 'description',
-    key: 'description',
-  },
-];
+	{
+		title: 'Title',
+		dataIndex: 'title',
+		key: 'title',
+	},
+	{
+		title: 'Description',
+		dataIndex: 'description',
+		key: 'description',
+	},
+]
 
 const ExpensesPage = () => {
-  return (
-    <Layout>
-      <ExpenseLayout title="เริ่มคำนวณวางแผนการเงินให้ลูกในอนาคตของคุณ">
-        <div>
-          {/* receive user income UI here */}
-        </div>
-      </ExpenseLayout>
-
-    </Layout>
-  )
+	return (
+		<Layout>
+			<ExpenseLayout title="เริ่มคำนวณวางแผนการเงินให้ลูกในอนาคตของคุณ">
+				<div>{/* receive user income UI here */}</div>
+			</ExpenseLayout>
+		</Layout>
+	)
 }
 
 export default ExpensesPage
--- src/pages/expenses/2/index.tsx
+++ src/pages/expenses/2/index.tsx
@@ -1,16 +1,16 @@
-import { Layout } from "~/components/Layout"
-import { ExpenseLayout } from "~/components/Layout/ExpenseLayout"
+import { Layout } from '~/components/Layout'
+import { ExpenseLayout } from '~/components/Layout/ExpenseLayout'
 
 const ExpensesPage = () => {
-    return (
-        <Layout>
-            <ExpenseLayout title="วางแผนการเงิน">
-                {
-                    // Insert your UI here
-                }
-            </ExpenseLayout>
-        </Layout>
-    )
+	return (
+		<Layout>
+			<ExpenseLayout title="วางแผนการเงิน">
+				{
+					// Insert your UI here
+				}
+			</ExpenseLayout>
+		</Layout>
+	)
 }
 
 export default ExpensesPage
