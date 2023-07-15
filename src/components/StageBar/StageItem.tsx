57d228c2e531fce9cca6dfdfa0a8b56f87f232ea
Nut Pinyo <bomb.np@gmail.com>
2023-07-15 23:12:14 +0700

    stageitem use data from props

--- src/components/StageBar/StageItem.tsx
+++ src/components/StageBar/StageItem.tsx
@@ -1,20 +1,33 @@
-import { type FC } from "react";
+import { type FC } from 'react'
 
 export interface StageItemProps {
-  className?: string
-  title: string
-  description: string
-  isActive?: boolean
+	className?: string
+	title: string
+	description: string
+	isActive?: boolean
 }
 
-export const StageItem: FC<StageItemProps> = ({ className = "", title, description, isActive = false}) => {
-  return (
-    <div className={`${className} p-4 flex flex-row space-x rounded-2xl justify-start gap-5 ${isActive ? "bg-zinc-600" : "bg-zinc-100"}`}>
-      <div className="rounded-full w-20 h-20 bg-zinc-300" />
-      <div className="flex flex-col justify-center gap-2">
-        <div className={`text-2xl font-semibold ${isActive ? "text-white" : "text-black"}`}>ขั้นตอนที่ 1</div>
-        <div className={`text-xl font-normal ${isActive ? "text-white" : "text-black"}`}>กรอกข้อมูลรายได้</div>
-      </div>
-    </div>
-  )
+export const StageItem: FC<StageItemProps> = ({
+	className = '',
+	title,
+	description,
+	isActive = false,
+}) => {
+	return (
+		<div
+			className={`${className} p-4 flex flex-row space-x rounded-2xl justify-start gap-5 ${
+				isActive ? 'bg-zinc-600' : 'bg-zinc-100'
+			}`}
+		>
+			<div className="rounded-full w-20 h-20 bg-zinc-300" />
+			<div className="flex flex-col justify-center gap-2">
+				<div className={`text-2xl font-semibold ${isActive ? 'text-white' : 'text-black'}`}>
+					{title}
+				</div>
+				<div className={`text-xl font-normal ${isActive ? 'text-white' : 'text-black'}`}>
+					{description}
+				</div>
+			</div>
+		</div>
+	)
 }
--- src/pages/story.tsx
+++ src/pages/story.tsx
@@ -4,7 +4,7 @@ const StoryPage = () => {
 	return (
 		<div className="flex flex-col space-y-4 p-8">
 			<StageItem className="w-72" title="ขั้นตอนที่ 1" description="กรอกข้อมูลรายได้" />
-			<StageItem className="w-72" title="ขั้นตอนที่ 1" description="กรอกข้อมูลรายได้" isActive />
+			<StageItem className="w-72" title="ขั้นตอนที่ 2" description="วางแผนการเงิน" isActive />
 		</div>
 	)
 }
