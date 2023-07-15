5ac12b4a09ae2e0aa1202638736dccc9ddb2f1ac
Supratouch <supratouchgussii@gmail.com>
2023-07-15 16:39:26 +0700

    init: expense

---
+++ src/logics/expenses.ts
@@ -0,0 +1,14 @@
+export const expenses = {
+  antenatal_care: {
+    title: "ค่าฝากครรภ์",
+    description: "",
+    choices: [
+      {
+        title: "",
+        description: "",
+        imageURL: "",
+        price: 0,
+      },
+    ],
+  },
+};
