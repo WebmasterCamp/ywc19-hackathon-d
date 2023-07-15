98b003b9e798bf60478f096d437d0e66eb2fe597
Nut Pinyo <bomb.np@gmail.com>
2023-07-15 16:32:56 +0700

    add prettier config

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,12 +1,10 @@
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from 'zod'
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-});
+	hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+		return {
+			greeting: `Hello ${input.text}`,
+		}
+	}),
+})
