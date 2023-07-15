cb80139c91945c7fd846a58a10b00be011c08b95
Nut Pinyo <bomb.np@gmail.com>
2023-07-15 16:37:37 +0700

    add eslint plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,24 +1,18 @@
 /** @type {import('@trivago/prettier-plugin-sort-imports').PrettierConfig} */
 const config = {
-  plugins: [
-    require.resolve("prettier-plugin-tailwindcss"), 
-    require.resolve("@trivago/prettier-plugin-sort-imports")
-  ],
-  useTabs: true,
-  singleQuote: true,
-  semi: false,
-  printWidth: 100,
-  trailingComma: "es5",
-  importOrder: [
-    "^react",
-    "<THIRD_PARTY_MODULES>",
-    "^@/",
-    "^[.]",
-    "^[.][.]",
-  ],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderParserPlugins: ["decorators-legacy", "jsx", "typescript"],
-};
+	plugins: [
+		require.resolve('prettier-plugin-tailwindcss'),
+		require.resolve('@trivago/prettier-plugin-sort-imports'),
+	],
+	useTabs: true,
+	singleQuote: true,
+	semi: false,
+	printWidth: 100,
+	trailingComma: 'es5',
+	importOrder: ['^react', '<THIRD_PARTY_MODULES>', '^@/', '^[.]', '^[.][.]'],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderParserPlugins: ['decorators-legacy', 'jsx', 'typescript'],
+}
 
-module.exports = config;
+module.exports = config
