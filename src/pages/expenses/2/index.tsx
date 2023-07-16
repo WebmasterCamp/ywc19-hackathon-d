820131cf56cad95be7ece7eb739016fa4c7fc3d4
Nut Pinyo <bomb.np@gmail.com>
2023-07-16 11:43:31 +0700

    add selection

--- src/pages/expenses/2/index.tsx
+++ src/pages/expenses/2/index.tsx
@@ -1,3 +1,4 @@
+import { atom, useAtom } from 'jotai'
 import { ExpenseLayout } from '~/components/Layout/ExpenseLayout'
 import { Layout } from '~/components/Layout/Layout'
 import { Steps, type StepsProps } from '~/components/Step/Steps'
@@ -8,14 +9,24 @@ import { expensesSteps } from '../1'
 
 const items: StepsProps['items'] = expensesSteps
 
+const selectedPhaseAtom = atom(0)
+
 const ExpensesPage = () => {
+	const [selectedPhase, setSelectedPhase] = useAtom(selectedPhaseAtom)
 	return (
 		<Layout>
 			<ExpenseLayout title="วางแผนการเงินให้ลูก">
 				<Steps items={items} activeStep={2} />
 				<div className="flex flex-col items-center gap-12 mt-12 px-24">
-					{phases.map((phase) => (
-						<ExpensePhase key={phase.title} phase={phase} />
+					{phases.map((phase, index) => (
+						<ExpensePhase
+							key={phase.title}
+							phase={phase}
+							selected={selectedPhase === index}
+							setSelected={() => {
+								setSelectedPhase(index)
+							}}
+						/>
 					))}
 				</div>
 			</ExpenseLayout>
