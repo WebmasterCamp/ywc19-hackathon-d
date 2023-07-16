64c1110812f811e46268d28232ab46700b69e2cb
Nut Pinyo <bomb.np@gmail.com>
2023-07-16 10:25:59 +0700

    reduce width

--- src/components/pages/expenses/2/NewCardButton.tsx
+++ src/components/pages/expenses/2/NewCardButton.tsx
@@ -10,7 +10,7 @@ export const NewCardButton: FC<NewCardButtonProps> = ({ onClick }) => {
 	return (
 		<div
 			onClick={onClick}
-			className="aspect-square rounded-lg border-2 border-dashed border-neutral-400 inline-flex flex-col justify-center items-center gap-2 cursor-pointer"
+			className="aspect-square rounded-lg border border-dashed border-neutral-400 inline-flex flex-col justify-center items-center gap-2 cursor-pointer"
 		>
 			<PlusCircleOutlined className="text-2xl" />
 			<div className=" text-gray-900 text-lg font-semibold leading-[27px]">เพิ่ม</div>
