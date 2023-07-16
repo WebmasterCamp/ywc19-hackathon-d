3b96a54cd0e991866016b7e3e9c946c0b5ea98a7
Nut Pinyo <bomb.np@gmail.com>
2023-07-16 10:05:41 +0700

    fix: first page redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react"
+import { useEffect } from 'react'
 
 export default function Home() {
-  useEffect(() => {
-    window.location.href = "/expenses"
-  }, [])
+	useEffect(() => {
+		window.location.href = '/expenses/1'
+	}, [])
 	return null
 }
