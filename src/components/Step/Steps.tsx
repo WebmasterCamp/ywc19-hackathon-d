661bab2cd755159ff1e8a4e9451fecf6b821930c
Supratouch <supratouchgussii@gmail.com>
2023-07-16 09:45:37 +0700

    fix: step

--- src/components/Step/Steps.tsx
+++ src/components/Step/Steps.tsx
@@ -8,7 +8,7 @@ export const Steps: FC<StepsProps> = ({ items }) => {
         <div className="flex justify-center items-center gap-x-4">
             {
                 items.map((item, index) => (
-                    <StepItem key={index} active={item.active} prompt={item.prompt} step={index} />
+                    <StepItem key={index} active={item.active} prompt={item.prompt} step={item.step} />
                 ))
             }
         </div>
